651b14b6e5b4c660a3f376fa1bafffd97875ad7e
Stanley <stas1498@icloud.com>
2024-02-23 14:59:07 +0100

    Final version

--- logics.js
+++ logics.js
@@ -1,35 +1,17 @@
+console.log("JavaScript файл загружен");
 
-document.addEventListener("DOMContentLoaded", function() 
-{
-    const pages = document.querySelectorAll('.page');
-    pages.forEach(page => page.style.display = 'none');
-    pages[0].style.display = 'block';
-    setButtonStyle(1, true);
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("DOM готов к использованию");
+  const navLinks = document.querySelectorAll("nav ul li a");
+  navLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      console.log("Нажата ссылка");
+      event.preventDefault(); // Отмена действия по умолчанию (перехода по ссылке)
+      const href = this.getAttribute("href"); // Получаем значение атрибута href
+      if (href) {
+        console.log("Перенаправление на страницу:", href);
+        window.location.href = href; // Перенаправляем пользователя на новую страницу
+      }
+    });
+  });
 });
-
-function onPageChange(pageNumber) 
-{
-    const pages = document.querySelectorAll('.page');
-    pages.forEach(page => page.style.display = 'none');
-
-    const nextPage = document.getElementById('page' + pageNumber);
-    nextPage.style.display = 'block';
-
-    const buttons = document.querySelectorAll('.navigation button');
-    buttons.forEach(button => setButtonStyle(button.id.substring(7), false));
-
-    setButtonStyle(pageNumber, true);
-}
-
-function setButtonStyle(buttonNumber, isActive = false) 
-{
-    const button = document.getElementById('btnPage' + buttonNumber);
-    button.style.backgroundColor = isActive ? '#FFD700' : '#007BFF';
-    button.style.color = isActive ? '#000' : '#fff';
-}
-
-function openImage() 
-{
-	var imgg = document.getElementById("imageID");
-	imgg.style.display = "block";
-}
